3b037e9e5a375ae5a58dc5e862d7ab6a55d1d347
cchannon <channon.charles@gmail.com>
2015-08-06 15:53:04 -0400

    updated plot 4 R file and png to edit a scaling issue in legend text

--- plot4.R
+++ plot4.R
@@ -1,3 +1,8 @@
+## While the other plot functions displayed the plot in addition to generating the png,
+## I had issues with the text scaling incorrectly when attempting to copy the plots from 
+## Windows device to png in the case of this plot.  Therefore, this one just creates the 
+## png directly, rather than displaying it and copying to png
+
 library(dplyr)
 powerData<- read.table("household_power_consumption.txt", na.strings = "?", comment.char = "", stringsAsFactors = FALSE, sep = ";", row.names = NULL, col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), skip = 1, colClasses = c("character", "character", rep("numeric", 7)))
 powerData <- powerData%>%
@@ -5,6 +10,7 @@ powerData <- powerData%>%
     mutate(datetime = paste(Date, Time, sep = " "))%>%
     select(c(datetime, Global_active_power:Sub_metering_3))%>%
     mutate(datetime = as.POSIXct(strptime(datetime, format = "%d/%m/%Y %H:%M:%S", tz = "")))
+png(file = "plot4.png")
 par(mfrow = c(2, 2))
 with(powerData, plot(datetime, Global_active_power, type = "n", ylab = "Global Active Power (kilowatts)"))
 with(powerData, lines(datetime, Global_active_power))
@@ -14,8 +20,7 @@ with(powerData, plot(datetime, Sub_metering_1, type = "n", ylab = "Energy sub me
 with(powerData, lines(datetime, Sub_metering_1, col = "black"))
 with(powerData, lines(datetime, Sub_metering_2, col = "red"))
 with(powerData, lines(datetime, Sub_metering_3, col = "blue"))
-legend("topright", pch = "", lty = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", pch = "", lty = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
 with(powerData, plot(datetime, Global_reactive_power, type = "n"))
 with(powerData, lines(datetime, Global_reactive_power))
-dev.copy(png, file = "plot4.png", height = 480, width = 480)
 dev.off()
